eb66cf201360e1cca5367397a979bf09e7e922a0
mdecleir <mdecleir@stsci.edu>
2024-06-04 14:21:33 -0400

    Added function to compare background

--- etc_testing.py
+++ etc_testing.py
@@ -37,14 +37,8 @@ def run_etc(datapath, outpath, star, filters, input_dic):
     """
     # create a Table for the results
     result_tab = Table(
-        names=(
-            "filter",
-            "SNR",
-        ),
-        dtype=(
-            "str",
-            "float64",
-        ),
+        names=("filter", "SNR", "background"),
+        dtype=("str", "float64", "float64"),
     )
 
     for filter in filters:
@@ -131,6 +125,7 @@ def run_etc(datapath, outpath, star, filters, input_dic):
             (
                 report["scalar"]["filter"],
                 report["scalar"]["sn"],
+                report["scalar"]["background"],
             )
         )
 
@@ -223,14 +218,147 @@ def comp_snr(datapath, outpath, star, filters, etc_snrs):
         overwrite=True,
     )
 
+    return result_tab
+
+
+def comp_back(datapath, outpath, star, filters, comp_tab, etc_res):
+    """
+    Function to compare the predicted and the measured background
+
+    Parameters
+    ----------
+    datapath : string
+        Path to the data files
+
+    outpath : string
+        Path to store the output table and plot
+
+    star : string
+        Name of the star
+
+    filters : list
+        Filters
+
+    comp_tab : astropy table
+        Comparison between predicted and measured SNR
+
+    etc_res : astropy.table
+        ETC predictions
+
+    Returns
+    -------
+    comp_tab : astropy table
+        Comparison between predicted and measured SNR and background
+    """
+    # create a figure
+    fig, ax = plt.subplots()
+
+    # open the file with the time dependent calibration factors
+    cal_tab = Table.read(datapath + "jwst_miri_photom_coeff.dat", format="ascii")
+
+    # add columns to the comparison table
+    comp_tab["bkg_ETC"] = np.full(len(comp_tab), np.nan)
+    comp_tab["bkg_data"] = np.full(len(comp_tab), np.nan)
+    comp_tab["bkg_diff=(data-ETC)/data"] = np.full(len(comp_tab), np.nan)
+
+    for i, filter in enumerate(filters):
+        # obtain the table with the photometry
+        filename = filter + "_eefrac0.7_phot.fits"
+        phot_tab = Table.read(datapath + filename)
+        star_mask = phot_tab["name"] == star
+
+        # skip this filter if there are no observations
+        if np.sum(star_mask) == 0:
+            continue
+
+        # obtain the MJD times of the observations and remove commissioning data
+        times = phot_tab[star_mask]["timemid"]
+        time_mask = times > 59762.2
+
+        # obtain the background of the observations
+        bkgs_DNs = phot_tab[star_mask][time_mask]["mean_bkg"]
+
+        # obtain the subarray of the observations
+        subs = phot_tab[star_mask][time_mask]["subarray"]
+        if not np.all(subs == subs[0]):
+            print(
+                "Subarray for this star is not the same between different observations, please check."
+            )
+        else:
+            sub = subs[0]
+
+        # calculate the calibration factor (Gordon+2024)
+        # CF(t) = {A + B exp[−(t−to)/τ]} D(SA)
+        # A, B and τ in table 4, Gordon+2024
+        # D(SA) in table 3, Gordon+2024
+        # to = 59720 d
+        if sub == "FULL" or sub == "BRIGHTSKY":
+            D = 1
+        elif sub == "SUB256":
+            D = 0.985
+        elif sub == "SUB128":
+            D = 0.977
+        elif sub == "SUB64":
+            D = 0.969
+        else:
+            print("Unknown subarray, please check.")
+        cal_fil = cal_tab[cal_tab["filter"] == filter]
+        CF = (
+            cal_fil["photmjysr"]
+            + cal_fil["amplitude"]
+            * np.exp(-(times[time_mask] - 59720) / cal_fil["tau"])
+        ) * D
+
+        # convert the units from DN/s/pix to MJy/sr and calculate the mean background
+        bkgs_MJysr = bkgs_DNs * CF
+        mean_bkg = np.mean(bkgs_MJysr)
+
+        # obtain the predicted background
+        etc_bkg = etc_res[etc_res["filter"] == filter.lower()]["background"][0]
+
+        # calculate the difference between the measured and predicted background
+        diff = (mean_bkg - etc_bkg) / mean_bkg
+
+        # save the results to the table
+        comp_tab["bkg_ETC"][comp_tab["filter"] == filter] = "%.2f" % etc_bkg
+        comp_tab["bkg_data"][comp_tab["filter"] == filter] = "%.2f" % mean_bkg
+        comp_tab["bkg_diff=(data-ETC)/data"][comp_tab["filter"] == filter] = (
+            "%.2f" % diff
+        )
+
+        # plot the measured and predicted background vs. time
+        sc = plt.scatter(
+            times[time_mask],
+            bkgs_MJysr,
+            label=filter + ": " + "{:.2f}".format(diff),
+        )
+        color = sc.get_facecolor()
+        plt.axhline(mean_bkg, color=color, ls=":")
+        plt.axhline(etc_bkg, color=color)
+
+    # finalize and save the figure
+    xticks = [59700, 59800, 59900, 60000, 60100, 60200, 60300, 60400]
+    xtick_times = Time(xticks, format="mjd")
+    xtick_labels = xtick_times.to_value("iso", subfmt="date")
+    plt.xticks(xticks, xtick_labels, rotation=20)
+    plt.axvline(60128, color="k")
+    plt.text(0.1, 0.7, "cycle 1", fontsize=18, transform=ax.transAxes)
+    plt.text(0.7, 0.7, "cycle 2", fontsize=18, transform=ax.transAxes)
+    plt.xlabel("date")
+    plt.ylabel("background (MJy/sr)")
+    plt.legend(loc="upper center", ncols=3)
+    plt.savefig(outpath + star + "_bkg.pdf", bbox_inches="tight")
+
+    return comp_tab
+
 
 def main():
     # define the path
     path = "/Users/mdecleir/Documents/MIRI_func/"
 
     # define the star
-    # star = "BD+60 1753"
-    star = "HD 180609"
+    star = "BD+60 1753"
+    # star = "HD 180609"
     # star = "2MASS J17430448+6655015"
 
     # open the original ETC input file (for BD+60 1753, filter F560W)
@@ -254,7 +382,19 @@ def main():
     etc_results = run_etc(path + "Absfluxcal/", path, star, filters, input_dic)
 
     # compare the predicted SNR from the ETC to the observed SNR
-    comp_snr(path + "Absfluxcal/", path, star, filters, etc_results)
+    comp_table = comp_snr(path + "Absfluxcal/", path, star, filters, etc_results)
+
+    # compare the predicted background from the ETC to the observed background
+    comp_table = comp_back(
+        path + "Absfluxcal/", path, star, filters, comp_table, etc_results
+    )
+
+    # write the results table to a file
+    comp_table.write(
+        path + star + "_comp.txt",
+        format="ascii",
+        overwrite=True,
+    )
 
 
 if __name__ == "__main__":
